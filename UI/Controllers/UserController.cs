926564110b6a800f25d8dddd0a76633441c47475
Deldar777 <54842815+Deldar777@users.noreply.github.com>
2020-10-04 13:04:53 +0200

    userContoller

---
+++ UI/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NoSQL.Models;
+
+namespace NoSQL.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Users()
+        {
+            List<User> users = new List<User>();
+
+            users.Add(new User() { FirstName = "Piet", LastName = "Paulsma", EmailAddress = "piet.paulsma@gmail.com", Location = UserLocation.Amsterdam, Type = UserType.Employee, Password = "12345" });
+
+            return View(users);
+        }
+    }
+}
