26d56e43e82fdbea07e3e1c8d87dae2429ff4bae
lyntier <64606744+Lyntier@users.noreply.github.com>
2020-10-20 11:39:29 +0200

    Add LoginService

---
+++ Services/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NoSQL.Models;
+
+namespace NoSQL.Services
+{
+    public interface ILoginService
+    {
+        bool Login(string emailAddress, string password, out User user);
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using NoSQL.DataAccess;
+using NoSQL.Models;
+
+namespace NoSQL.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IRepository<User> _userRepository;
+
+        public LoginService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public bool Login(string emailAddress, string password, out User user)
+        {
+            user = _userRepository.FindOne(u =>
+                u.EmailAddress.Equals(emailAddress) &&
+                u.Password.Equals(password)
+            );
+
+            return user; // implicit true if not null
+        }
+    }
+}
